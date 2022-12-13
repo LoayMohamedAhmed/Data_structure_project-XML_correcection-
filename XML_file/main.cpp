e44e9193469ef8c43084c755beccc1b01ee8e67d
renal-tarek <renaltarek287@gmail.com>
2022-12-13 23:57:29 +0200

    2 functions

--- XML_file/main.cpp
+++ XML_file/main.cpp
@@ -9,6 +9,30 @@
 using namespace std;
 using namespace std;
 
+int order(char* data,int i)
+{
+    int count1=0;
+    int j=0;
+    while(j<i)
+    {
+        if(data[j]=='\n')
+            count1++;
+        j++;
+    }
+    return count1;
+}
+
+
+
+string conv_to_string(char characters[],int size1)
+{
+    string s="";
+    for(int i=0;i<size1;i++)
+    {
+        s+=characters[i];
+    }
+    return s;
+}
 int main()
 {
     cout << "Hello world!" << endl;
