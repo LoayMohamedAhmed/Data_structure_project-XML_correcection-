4a0d83131fbc42a7f0cca3589ac8d78cb84193ae
nayera540 <69148381+nayera540@users.noreply.github.com>
2023-01-15 19:49:41 +0200

    reverse the message

--- GUI/Project-GUI/XML.h
+++ GUI/Project-GUI/XML.h
@@ -9,11 +9,17 @@
 #include <QString>
 
 using namespace std;
-
 typedef struct{
     int index;
     string s;
 } tag;
+
+int previous = 0;     //For storing the previous stack page
+int max_size=0;     //For storing the size of the input file
+QString filename;   //For storing the path in the path
+QString text;      //For storing the content of the file in QString
+string q;
+
 
 int order(string data,int i)
 {
@@ -27,8 +33,6 @@ int order(string data,int i)
     }
     return count1;
 }
-
-
 
 string conv_to_string(char characters[],int size1)
 {
@@ -51,6 +55,156 @@ bool compare1(string s1, string stk)
     }
     return check;
 }
+
+QString check_errors(string data)
+{
+
+    stack<char> characters;
+    stack<tag> errors_miss;
+    stack<tag> errors_miss1;
+    stack<tag> errors_wrong_poss;
+    stack<tag> errors_wrong_poss1;
+    stack<string> reverse_msg;
+    deque<tag> tags;
+    deque<tag> temp;
+    string message;
+    int i =0;
+    tag T;
+    int mark = 0;
+    int k = 50;
+
+    while(i<data.size())
+    {
+
+        if(data[i]=='<')
+        {
+
+            int k=i;
+            mark++;
+            do
+            {
+                  characters.push(data[k]);
+                  k++;
+
+            }
+            while(data[k]!='>');
+            characters.push(data[k]);
+            int size1 = characters.size() - 1;
+            char temp_array[size1 + 1];
+
+
+
+            for (int j = size1; j >= 0; j--)
+            {
+                temp_array[j] = characters.top();
+                characters.pop();
+            }
+            T.s = conv_to_string(temp_array, size1 + 1);
+            T.index = k;
+            if (mark==1)
+            {
+                tags.push_front(T);
+            }
+
+            if (!tags.empty() && mark != 1)
+            {
+                if (compare1(T.s, tags.front().s))
+                {
+
+                    tags.pop_front();
+                }
+                else
+                {
+                    tags.push_front(T);
+                }
+            }
+            i = k;
+        }
+        i++;
+    }
+    if(tags.empty())
+    {
+
+        message+="No errors found,the file is under control :)";
+
+    }
+    else
+    {
+
+        while(!tags.empty())
+        {
+            if(compare1(tags.front().s,tags.back().s))
+            {
+                tags.pop_front();
+                tags.pop_back();
+            }
+            else
+            {
+                while(!tags.empty())
+                {
+                    if(compare1(tags.front().s,tags.back().s))
+                    {
+                       errors_wrong_poss.push(tags.back());
+                       errors_wrong_poss.push(tags.front());
+                       tags.pop_front();
+                       tags.pop_back();
+                       break;
+                    }
+                    else
+                    {
+                        if (tags.size() == 1)
+                        {
+                            errors_miss.push(tags.front());
+                            tags.pop_front();
+                            break;
+                        }
+                        temp.push_front(tags.front());
+                        tags.pop_front();
+                    }
+                }
+                while(!temp.empty())
+                {
+
+                    tags.push_front(temp.front());
+                    temp.pop_front();
+                }
+            }
+        }
+
+
+        while(!errors_miss.empty())
+        {
+
+            errors_miss1.push(errors_miss.top());
+            int num =order(data,errors_miss.top().index)+1;
+            reverse_msg.push("You have an error in line " + to_string(num) + " at tag (" + errors_miss.top().s+")\n");
+            errors_miss.pop();
+
+        }
+
+        while (!errors_wrong_poss.empty())
+        {
+
+            errors_wrong_poss1.push(errors_wrong_poss.top());
+            int num = order(data, errors_wrong_poss.top().index) + 1;
+            reverse_msg.push("You have an error in line " + to_string(num) + " at tag (" + errors_wrong_poss.top().s +")\n");
+            errors_wrong_poss.pop();
+        }
+
+
+    }
+    while(!reverse_msg.empty()){
+        message += reverse_msg.top();
+        reverse_msg.pop();
+    }
+
+
+    return QString::fromStdString(message);
+}
+
+
+
+
 
 
 
--- GUI/Project-GUI/mainwindow.cpp
+++ GUI/Project-GUI/mainwindow.cpp
@@ -7,10 +7,6 @@
 #include <iostream>
 #include <XML.h>
 
-int previous = 0;     //For storing the previous stack page
-int max_size=0;     //For storing the size of the input file
-QString filename;   //For storing the path in the path
-QString text;      //For storing the content of the file in QString
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -20,148 +16,9 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     ui->stackedWidget->setCurrentIndex(0);
 
+
 }
 
-
-QString check_errors(string data)
-{
-
-    stack<char> characters;
-    stack<tag> errors_miss;
-    stack<tag> errors_miss1;
-    stack<tag> errors_wrong_poss;
-    stack<tag> errors_wrong_poss1;
-    deque<tag> tags;
-    deque<tag> temp;
-    string message;
-    int i =0;
-    tag T;
-    int mark = 0;
-    int k = 50;
-
-    while(i<data.size())
-    {
-
-        if(data[i]=='<')
-        {
-
-            int k=i;
-            mark++;
-            do
-            {
-                  characters.push(data[k]);
-                  k++;
-
-            }
-            while(data[k]!='>');
-            characters.push(data[k]);
-            int size1 = characters.size() - 1;
-            char temp_array[size1 + 1];
-
-
-
-            for (int j = size1; j >= 0; j--)
-            {
-                temp_array[j] = characters.top();
-                characters.pop();
-            }
-            T.s = conv_to_string(temp_array, size1 + 1);
-            T.index = k;
-            if (mark==1)
-            {
-                tags.push_front(T);
-            }
-
-            if (!tags.empty() && mark != 1)
-            {
-                if (compare1(T.s, tags.front().s))
-                {
-
-                    tags.pop_front();
-                }
-                else
-                {
-                    tags.push_front(T);
-                }
-            }
-            i = k;
-        }
-        i++;
-    }
-    if(tags.empty())
-    {
-
-        message+="No errors found";
-
-    }
-    else
-    {
-
-        while(!tags.empty())
-        {
-            if(compare1(tags.front().s,tags.back().s))
-            {
-                tags.pop_front();
-                tags.pop_back();
-            }
-            else
-            {
-                while(!tags.empty())
-                {
-                    if(compare1(tags.front().s,tags.back().s))
-                    {
-                       errors_wrong_poss.push(tags.back());
-                       errors_wrong_poss.push(tags.front());
-                       tags.pop_front();
-                       tags.pop_back();
-                       break;
-                    }
-                    else
-                    {
-                        if (tags.size() == 1)
-                        {
-                            errors_miss.push(tags.front());
-                            tags.pop_front();
-                            break;
-                        }
-                        temp.push_front(tags.front());
-                        tags.pop_front();
-                    }
-                }
-                while(!temp.empty())
-                {
-
-                    tags.push_front(temp.front());
-                    temp.pop_front();
-                }
-            }
-        }
-
-
-        while(!errors_miss.empty())
-        {
-
-            errors_miss1.push(errors_miss.top());
-            int num =order(data,errors_miss.top().index)+1;
-            //cout<<"you have an error in line "<<num<<"at tag ("<<errors_miss.top().s<<")"<<endl;
-            message += "You have an error in line " + to_string(num) + " at tag (" + errors_miss.top().s+")\n";
-            errors_miss.pop();
-        }
-        while (!errors_wrong_poss.empty())
-        {
-
-            errors_wrong_poss1.push(errors_wrong_poss.top());
-            int num = order(data, errors_wrong_poss.top().index) + 1;
-            //cout << "you have an error in line " << num << "at tag (" << errors_wrong_poss.top().s << ")" << endl;
-            message += "You have an error in line " + to_string(num) + " at tag (" + errors_wrong_poss.top().s +")\n";
-            errors_wrong_poss.pop();
-        }
-
-    }
-
-
-    return QString::fromStdString(message);
-}
 
 MainWindow::~MainWindow()
 {
@@ -185,19 +42,9 @@ void MainWindow::on_radioButton_clicked()
 
 
 
-string q;
-QString browse_error_message;
 void MainWindow::on_pushButton_clicked()
 {
     filename = QFileDialog::getOpenFileName(this,tr("Choose"),"c://","XML File (*.XML)");
-        //QFile file (QFileDialog::getOpenFileName(
-        //            this,
-        //            tr("Choose"),
-        //            "c://",
-        //            "Text File (*.XML)"
-        //            ));
-
-
     QTextStream in(&filename);
     ui->lineEdit->setText(in.readAll());
     QString file1 = ui->lineEdit->text();
@@ -208,12 +55,10 @@ void MainWindow::on_pushButton_clicked()
     q = text.toStdString();
     QTextStream myfile_stream(&file2);
     ui->textBrowser->setText(text);
-    browse_error_message = check_errors(q);
 
 }
 
-QString text_error_message;
-string text_q;
+
 void MainWindow::on_pushButton_2_clicked()
 {
 
@@ -228,13 +73,9 @@ void MainWindow::on_pushButton_2_clicked()
 
     }
     file.close();
-    //max_size = file.size();
     QTextStream out2(&file);
     ui->textBrowser->setText(inputText);
-    text_q = inputText.toStdString();
-    browse_error_message = check_errors(text_q);
-    max_size = text_q.size();
-    //Go to options page
+    q = inputText.toStdString();
     ui->stackedWidget->setCurrentIndex(3);
 }
 
@@ -303,12 +144,9 @@ void MainWindow::on_pushButton_11_clicked()
 
 
 //Detecting Errors Button
-
-
-
 void MainWindow::on_pushButton_3_clicked()
 {
-    QMessageBox::warning(this,"Error Detecting Window",browse_error_message);
+    QMessageBox::warning(this,"Error Detecting Window",check_errors(q));
 }
 
 
