8689f512604bf19eb83afce0fb5b85656439eedf
Renal-Tarek <104008582+Renal-Tarek@users.noreply.github.com>
2023-02-02 22:21:14 +0200

    Update header.h
    update header

--- XML_file/include/header.h
+++ XML_file/include/header.h
@@ -1,18 +1,26 @@
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <stack>
-#include <string.h>
+#include <string>
 #include <deque>
 #include "string.h"
-
+#include "node.h"
+#include "tree.h"
+#include "Graph.h"
+#include <cstdlib>
+#include <vector>
 using namespace std;
+
 
 typedef struct{
     int index_end;
     int index_start;
-    string s;
+    std::string s;
 } tag;
+
+
 
 int file_size(string path );
 void read_file(char data[], string path);
@@ -23,3 +31,11 @@ void check_errors(char data[],int max_size);
 void error_correction(char* data,int size1);
 void minifying(char* data, int size1);
 void prettifying(char* data,int size1);
+deque<string> intoQueue(char data[],int max_size);
+void convert_jason(char data[],int max_size);
+Graph build_graph(char data[], int max_size);
+vector<string> bodySearch(node *node);
+vector<string> searchTopic(string word, bool choice);
+bool compare2(string s1, string s2);
+
+
