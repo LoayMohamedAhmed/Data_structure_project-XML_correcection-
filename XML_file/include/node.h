293f1bc1ac8c616f8e60c5f29471d38c22418d6a
LoayMohamedAhmed <101834345+LoayMohamedAhmed@users.noreply.github.com>
2023-02-02 20:43:22 +0200

    Update node.h
    small edit

--- XML_file/include/node.h
+++ XML_file/include/node.h
@@ -11,9 +11,10 @@ class node
         string data;
         node* children[max_child];
         int child_num;
+        int id;
         node(string data);
         node();
-
+        int num_siblings;
     friend class tree;
 };
 
