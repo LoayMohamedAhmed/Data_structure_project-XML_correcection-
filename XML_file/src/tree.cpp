9441c0986ad52759f478df7e59490f227203bffc
LoayMohamedAhmed <101834345+LoayMohamedAhmed@users.noreply.github.com>
2023-02-02 20:40:01 +0200

    Update tree.cpp
    edit 1

--- XML_file/src/tree.cpp
+++ XML_file/src/tree.cpp
@@ -1,5 +1,6 @@
-#include "../include/tree.h"
-#include "../include/node.h"
+
+#include "tree.h"
+#include "node.h"
 
 deque<string> data_update;
 bool compare(string s1,string stk)
@@ -26,19 +27,18 @@ tree::tree()
     this->root=NULL;
 }
 
-node *tree::add_child(node *root1,deque<string>data)
+node *tree::add_child(node *root1,deque<string>data, int no_siblings)
 {
 
-//    if(is_empty1())
-//        this->root=new node(data.front());
-    cout<<"data: "<<data.front()<<endl;
+    //cout<<"data: "<<data.front()<<endl;
+  // cout<<"data: "<<data.front()<<endl;
     while(data.front()[1]=='/')
     {
         data.pop_front();
     }
 
     root1=new node(data.front());
-    cout<<root1->data<<endl;
+    //cout<<root1->data<<endl;
     int count1=0;
 
     string X= data.front();
@@ -46,7 +46,7 @@ node *tree::add_child(node *root1,deque<string>data)
 
     if(data.front()[1]=='/')
     {
-        cout<<"BASE"<<endl;
+        //cout<<"BASE"<<endl;
         data_update=data;
         return root1;
 
@@ -57,6 +57,10 @@ node *tree::add_child(node *root1,deque<string>data)
     while(!compare(temp.front(),X))
     {
         temp.pop_front();
+        if(Y[0]!='<')
+        {
+            count1+=1;
+        }
         if(compare(temp.front(),Y))
         {
             count1+=1;
@@ -64,15 +68,17 @@ node *tree::add_child(node *root1,deque<string>data)
             Y=temp.front();
         }
     }
-    cout<<"temp: "<<temp.front()<<endl;
-    cout<<count1<<endl;
+//    cout<<"temp: "<<temp.front()<<endl;
+//    cout<<"num"<<count1<<endl;
     root1->child_num=count1;
+    root1->num_siblings=no_siblings;
 
     for(int i=0;i<count1;i++)
     {
-        root1->children[i]=add_child(root1->children[i],data);
-        data=data_update;
 
+        root1->children[i]=add_child(root1->children[i],data,count1);
+        data=data_update;
+        root1->children[i]->id=i+1;
     }
 
     return root1;
@@ -80,7 +86,9 @@ node *tree::add_child(node *root1,deque<string>data)
 }
 void tree::print(node*root1)
 {
-    std::cout<<root1->data<<"\t"<<root1->child_num<<endl;
+    std::cout<<root1->data<<"\t"<<root1->id<<"no_sib"<<root1->num_siblings<<endl;
+    //std::cout<<"no_sib"<<root1->num_siblings<<endl;
+
     if(!root1)
         return;
     for(int i=0;i<root1->child_num;i++)
@@ -91,3 +99,4 @@ tree::~tree()
 {
    delete root;
 }
+
